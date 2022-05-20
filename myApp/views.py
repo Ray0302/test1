fc49db34983f46c6794494459acda621cfe91ec9
Ray0302 <ray910302@gmail.com>
2022-05-20 17:00:48 +0800

    add a hello3.html

--- myApp/views.py
+++ myApp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 
-def sayhellow(resquest):
+def sayhello(resquest):
     return HttpResponse('sayhello......')
+    
 def hello2(resquest, username):
     now = datetime.now()
-    return render(request, 'hello3.html', {'username':username, 'now':now})
+    return render(resquest, 'hello3.html', {'username':username, 'now':now})
 
