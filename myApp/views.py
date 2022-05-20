3cb32a5a15e0df2cd87460a838ca5840173b6b4a
Ray0302 <ray910302@gmail.com>
2022-05-20 16:16:23 +0800

    add a hello3.html

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def sayhellow(resquest):
+    return HttpResponse('sayhello......')
+def hello2(resquest, username):
+    now = datetime.now()
+    return render(request, 'hello3.html', {'username':username, 'now':now})
+
--- test1/urls.py
+++ test1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myApp.views import hello2
+from myApp.views import sayhello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello2/<str:username>', hello2),
 ]
